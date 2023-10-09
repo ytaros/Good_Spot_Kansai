7fe0ec59224c398b6d7f43498adc79b3f738cf8f
ytaros <hoo.miii0616@gmail.com>
2023-10-09 15:28:12 +0900

    articleコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/new'
+  get 'articles/show'
+  get 'articles/edit'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/destroy'
   root to: "tops#top"
 
   get '/main', to: 'tops#index', as: 'main_top'
   resources :users
+  resources :articles
   get 'login', to: 'sessions#new'
   post 'login', to:'sessions#create'
   delete 'logout', to:'sessions#destroy'
