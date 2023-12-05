9a2d25eeab211cb9676a7487f4764de359356c3f
ytaros <hoo.miii0616@gmail.com>
2023-12-05 23:30:57 +0900

    デプロイ修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,4 @@ yarn install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:reset
