6e86bc6200947f00f9610897a16a73976e3eb39a
ytaros <hoo.miii0616@gmail.com>
2023-12-13 19:00:28 +0900

    🔧 TOPの修正、新規登録後の自動ログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] =  I18n.t("defaults.message.created", item: "User")
-      redirect_to login_path
+      redirect_to main_top_path
     else
       flash.now[:error] = @user.errors.full_messages.join(', ')
       render 'new', status: :unprocessable_entity
