2d678b9d0cb40907eeeb67aa74cb9e77887f3b8b
ytaros <hoo.miii0616@gmail.com>
2024-09-13 23:49:43 +0900

    fix: aws修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,7 +18,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: "ap-northeast-1",
+      region: "ap-northeast-3",
       path_style: true
     }
   end
